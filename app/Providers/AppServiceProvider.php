4002d37fe65de7665c7c3e7d563df36c8569cad6
Cerizz-CIeRa-Avel <maincrizztenance@gmail.com>
2026-06-30 09:55:58 +0700

    feat: implement register and login endpoints

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Rate Limiter untuk Login: Maksimal 5x per menit per IP/Email
+        RateLimiter::for('login', function (Request $request) {
+            return Limit::perMinute(5)->by($request->email ?: $request->ip());
+        });
     }
 }
